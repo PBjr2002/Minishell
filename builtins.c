e9dcd9269e7f9fce6027fc4085d5f3d81183bc49
Paulo Bernardo Jorge Ribeiro <pauberna@c2r6s6.42lisboa.com>
2024-09-16 17:39:57 +0100

    16/9

--- builtins.c
+++ builtins.c
@@ -6,58 +6,81 @@
 /*   By: pauberna <pauberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 14:30:34 by pauberna          #+#    #+#             */
-/*   Updated: 2024/09/16 16:02:08 by pauberna         ###   ########.fr       */
+/*   Updated: 2024/09/16 17:30:54 by pauberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	decider(char *cmd)
+void	decider(t_tree *tree, t_environment *envr)
 {
-	int		i;
+	t_tree	*tmp;
 
-	i = 0;
-	while (info->cmds[i])
+	tmp = tree;
+	if (ft_strcmp(tree->str, "echo") == 0)
 	{
+		tree = tree->right;
+		exec_echo(tree, tmp, envr);
 	}
-	
-	if (ft_strcmp(cmd, "echo") == 0)
-		exec_echo(fd, av, info);
-	else if (ft_strcmp(cmd, "cd") == 0)
+	else if (ft_strcmp(tree->str, "cd") == 0)
 		exec_cd(fd, av, info);
-	else if (ft_strcmp(cmd, "pwd") == 0)
+	else if (ft_strcmp(tree->str, "pwd") == 0)
 		exec_pwd(fd);
-	else if (ft_strcmp(cmd, "export") == 0)
+	else if (ft_strcmp(tree->str, "export") == 0)
 		exec_export(fd, av, info);
-	else if (ft_strcmp(cmd, "env") == 0)
+	else if (ft_strcmp(tree->str, "env") == 0)
 		exec_env(fd, info);
-	else if (ft_strcmp(cmd, "exit") == 0)
+	else if (ft_strcmp(tree->str, "exit") == 0)
 		prepare_exit(av, info);
-	else if (ft_strcmp(cmd, "unset") == 0)
+	else if (ft_strcmp(tree->str, "unset") == 0)
 		exec_unset(av, info);
 	else
 		exec_other(fd, av, info);
 }
 
-void	search_tree(t_tree *tree)
+void	search_tree(t_tree *tree, t_environment *envr)
 {
+	t_tree	*tmp;
+
 	while (tree)
 	{
-		while (tree && tree->type != 8)
+		while (tree && tree->type != 8 && tree->type != 10)
 			tree = tree->left;
 		if (tree)
 		{
+			tmp = tree;
 			if (tree->left)
 			{
-				while (tree)
+				while (tree->left)
 					tree = tree->left;
-				redirect_solver;
+				while (tree != tmp)
+				{
+					redirect_solver(tree, tmp);
+					tree->solved = true;
+					tree = tree->parent;
+				}
 			}
-			
-			decider(tree->str);
+			decider(tree);
 			tree->solved = true;
 		}
+		tree = tree->parent;
 	}
-	
-	
+}
+
+void	redirect_solver(t_tree *tree, t_tree *cmd)
+{
+	if (tree->type == SINGLE_IN)
+		tree->fd_in = open(tree->str, O_TRUNC);
+	else if (tree->type == SINGLE_OUT)
+		tree->fd_out = open(tree->str, O_TRUNC);
+	else if (tree->type == DOUBLE_IN)
+		tree->fd_in = open(tree->str, O_APPEND);
+	else if (tree->type == DOUBLE_OUT)
+		tree->fd_out = open(tree->str, O_APPEND);
+	if (tree->fd_in == -1 || tree->fd_out == -1)
+		printf("There was an error opening\n");
+	if (dup2(tree->fd_in, cmd->fd_in) == -1)
+		printf("There was an error redirecting\n");
+	if (dup2(tree->fd_out, cmd->fd_out) == -1)
+		printf("There was an error redirecting\n");
 }
