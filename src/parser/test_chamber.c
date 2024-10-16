e410e137caa9b1fa13262898eaece961290cb19f
PBjr2002 <paulobernardo.2002@gmail.com>
2024-10-16 17:33:23 +0100

    lets fucking goooo

--- src/parser/test_chamber.c
+++ src/parser/test_chamber.c
@@ -6,7 +6,7 @@
 /*   By: pauberna <pauberna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 13:50:25 by lmiguel-          #+#    #+#             */
-/*   Updated: 2024/10/16 17:31:44 by pauberna         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:32:58 by pauberna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,26 +52,4 @@
 
 	//ls | grep s >>out | wc <out | cat
 	TEST THIS COMMAND FOR LEAKS
-
-	ls"-l"|wc|grep|echo>>txt.txt<<EOF'A'"|">OUT
-
-	nem eu percebi o que era suposto acontecer aqui
-
-	supostamente, seria para testar o delimiter do heredoc...? n sei qual sera porque o nosso
-	nao manda msg de erro a indicar o delimiter quando mandamos ctrl + d
-
-	(o delimiter seria EOFA|)
-
-	testar a partir da linha 79 até a linha 85 o problema é o mesmo
-	estão a ser criados 2 redirects em vez de só um então o problema n é o heredoc mas sim o estar a ser feito 2x
-
-	(solved as fuck)
-
---------------------------------------------------------------------------
-
-	linha 81
-	linha 83
-	linha 84
-
--------------------------------------------------------------------------------
 */
