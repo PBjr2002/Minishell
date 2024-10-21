2298b054cc176993979470704ce44edfddc516ed
Lourenco Miguel Bouzas Saraiva <lmiguel-@c1r10s4.42lisboa.com>
2024-10-21 14:05:08 +0100

    fixed incorrect tree pointer if only sending redirections

--- src/parser/parsing.c
+++ src/parser/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lmiguel- <lmiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 13:30:15 by lmiguel-          #+#    #+#             */
-/*   Updated: 2024/10/18 15:21:53 by lmiguel-         ###   ########.fr       */
+/*   Updated: 2024/10/21 13:57:16 by lmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,8 @@ t_tree	*parsing(t_token *token_list, t_environment *env)
 	}
 	token_list = temp;
 	tree = tree_creation_function(token_list);
+	while (tree->parent)
+		tree = tree->parent;
 	return (tree);
 }
 
