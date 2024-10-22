5621a4ea3e8c82eeff303dad1c225490413d00ee
Lourenco Miguel Bouzas Saraiva <lmiguel-@c1r10s4.42lisboa.com>
2024-10-22 19:09:19 +0100

    where is my son

--- src/builtins/builtins_helper8.c
+++ src/builtins/builtins_helper8.c
@@ -6,7 +6,7 @@
 /*   By: lmiguel- <lmiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 15:21:37 by pauberna          #+#    #+#             */
-/*   Updated: 2024/10/22 19:07:13 by lmiguel-         ###   ########.fr       */
+/*   Updated: 2024/10/22 19:09:03 by lmiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,7 +61,6 @@ void	exec_cmd(t_tree *tree, t_environment *envr, int mode)
 {
 	if (!tree)
 		return ;
-	search_redirect(tree, envr, 0);
 	if (mode == 0)
 	{
 		if (search_redirect(tree, envr, 0, 0) == -1)
